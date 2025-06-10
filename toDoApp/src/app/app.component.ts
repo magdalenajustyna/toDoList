4232b5a1eae7d0b121d66580851576b73b1a3ad9
Nadja Stauber <nadja.stauberahoo.de>
2025-06-10 18:23:51 +0200

    filter input element erstellt

--- toDoApp/src/app/app.component.ts
+++ toDoApp/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { DeleteComponent } from './delete/delete.component';
 import { ArchivbuttonComponent } from './archivbutton/archivbutton.component';
+import { FilterComponent } from './filter/filter.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterFormComponent, DeleteComponent, ArchivbuttonComponent],
+  imports: [RouterOutlet, RegisterFormComponent, DeleteComponent, ArchivbuttonComponent, FilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
