9d46a60622445b501396d630ce6dbd4d2a00ba75
Magdalena Uçar <magdalena.ucar@posteo.de>
2025-06-10 20:57:51 +0200

    Login-Formular

--- toDoApp/src/app/app.component.ts
+++ toDoApp/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RegisterFormComponent } from './register-form/register-form.component';
 import { DeleteComponent } from './delete/delete.component';
 import { ArchivbuttonComponent } from './archivbutton/archivbutton.component';
 import { FilterComponent } from './filter/filter.component';
+import { LoginComponent } from './login/login.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterFormComponent, DeleteComponent, ArchivbuttonComponent, FilterComponent],
+  imports: [RouterOutlet, RegisterFormComponent, DeleteComponent, ArchivbuttonComponent, FilterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
