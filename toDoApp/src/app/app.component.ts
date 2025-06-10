9e1699c2162c47a7919e7c811da7cb8acb87dc72
Nadja Stauber <nadja.stauberahoo.de>
2025-06-10 17:22:55 +0200

    delete button erstellt

--- toDoApp/src/app/app.component.ts
+++ toDoApp/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterFormComponent } from './register-form/register-form.component';
+import { DeleteComponent } from './delete/delete.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegisterFormComponent],
+  imports: [RouterOutlet, RegisterFormComponent, DeleteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
