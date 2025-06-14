18f9393e60c6baeca1556f2b999101c1b3048912
Magdalena Uçar <magdalena.ucar@posteo.de>
2025-06-14 12:43:23 +0200

    home ueberarbeitet

--- toDoApp/src/app/home/home.component.ts
+++ toDoApp/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FilterComponent } from '../filter/filter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FilterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
