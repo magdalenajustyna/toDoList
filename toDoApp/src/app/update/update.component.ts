dd0ea4350d699649fa040d86dc18d1a5997151d0
Nadja Stauber <nadja.stauberahoo.de>
2025-07-08 19:09:10 +0200

    Datepicker auslesen funktioniert, Update-Methode funktioniert, Unterstützung durch ChatKI

--- toDoApp/src/app/update/update.component.ts
+++ toDoApp/src/app/update/update.component.ts
@@ -10,94 +10,74 @@ import { Todo } from '../shared/todo';
   standalone: true, //aus seinem Code übernommen
   imports: [RouterLink, HeaderComponent, ReactiveFormsModule],
   templateUrl: './update.component.html',
-  styleUrl: './update.component.css'
+  styleUrl: './update.component.css',
 })
 export class UpdateComponent implements OnInit {
- 
-  private dataservice = inject(BackendService)
-  private route = inject(ActivatedRoute)
-  private router = inject(Router) 
+  private dataservice = inject(BackendService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
   todo!: Todo;
-  id: string | null = ''
+  id: string | null = '';
   form = new FormGroup({
-    todoNameControl : new FormControl<string>(''),
+    //Steuerelemente
+    todoNameControl: new FormControl<string>(''),
     prioControlOld: new FormControl<string>(''),
     prioControlButton: new FormControl<string>(''),
     datumControlOld: new FormControl<string>(''),
-    datumControlButton: new FormControl<string>('')
-});
+    datumControlButton: new FormControl<string>(''),
+  });
 
-  ngOnInit(): void //holt todo aus Datenbank und befüllt Formular
-  {
+  ngOnInit(): void {
+    //holt todo aus Datenbank und befüllt Formular
     this.id = this.route.snapshot.paramMap.get('id');
-    console.log('id = ', this.id)
-    this.dataservice.getOne(this.id!)
-    .then(response => { 
-      this.todo = response 
-      this.form.patchValue({
-        todoNameControl: this.todo?.todoName,
-        prioControlOld: this.todo?.prio,
-        datumControlOld: this.todo?.datum
+    console.log('id = ', this.id);
+    this.dataservice
+      .getOne(this.id!)
+      .then((response) => {
+        this.todo = response;
+        this.form.patchValue({
+          todoNameControl: this.todo?.todoName,
+          prioControlOld: this.todo?.prio,
+          datumControlOld: this.todo?.datum,
+        });
+        return this.todo;
       })
-      return this.todo
-    })
-    .then( todo => {
-      if(!todo.todoName) {
-        this.router.navigate(['/home']);  // richtig??
-      } else {
-        console.log('todo in DetailComponent : ', todo )
-      }
-    })   
+      .then((todo) => {
+        if (!todo.todoName) {
+          this.router.navigate(['/home']); // richtig??
+        } else {
+          console.log('todo in DetailComponent : ', todo);
+        }
+      });
   }
 
-  setPrio(prio: string) : void {    //mit Hilfe von ChatKI
-
-    this.form.patchValue({ prioControlButton: prio });    //setzt PrioWert aus Dropdown ins Formular (zwischenspeichern)
-    
+  //mit Hilfe von ChatKI
+  setPrio(prio: string): void {
+    this.form.patchValue({ prioControlButton: prio }); //setzt PrioWert aus Dropdown ins Formular (zwischenspeichern)
   }
 
-  formatDateToString(date: Date): string {
-    const day = date.getDate();
-    const month = date.getMonth() + 1; // Monate sind nullbasiert -> Januar = 0
-    const year = date.getFullYear();
-
-    // Tag und Monat in 2-stelliges Format umwandeln
-    const dayString = day < 10 ? '0' + day : day.toString();
-    const monthString = month < 10 ? '0' + month : month.toString();
-    const datumNeu = dayString + '.' + monthString + '.' + year;
-    return datumNeu;
-    /* return `${dayString}.${monthString}.${year}`;*/
-    
-}
-
-  setDate(datum: Date) : void {    
-    //Datum aus Datepicker auslesen (YYYY-MM-DD) --> dieser muss in String DD-MM-YYYY umgewandelt werden
-    const datumNeu = this.formatDateToString(datum);   
-    this.form.patchValue({ datumControlButton: datumNeu });    //setzt Datum aus Dropdown ins Formular (zwischenspeichern)
-    
-    
+  //Methode um String umzusortieren   //Hilfe von Chat KI
+  formatDateString_DDMMYYYY(datum: string): string {
+    const [year, month, day] = datum.split('-');
+    return day + '.' + month + '.' + year;
   }
 
-  update(): void {
+  update(): void {      //FUNKTIONIERT!!
     const values = this.form.value;
-    this.todo.todoName = values.todoNameControl!;   
-    this.todo.prio =  values.prioControlButton!;    
-    this.todo.datum = values.datumControlButton!;
+    this.todo.todoName = values.todoNameControl!;
+    this.todo.prio = values.prioControlButton!;
 
-    this.dataservice.update(this.id!, this.todo)
-    .then( () => this.router.navigate(['/home']))     //geht nur zu Home wenn update erfolgreich
+    let datumNeu = this.formatDateString_DDMMYYYY(values.datumControlButton!);
+    console.log('Ausgabe des Datum als String: ', datumNeu);
+    this.todo.datum = datumNeu;
+
+    this.dataservice
+      .update(this.id!, this.todo)
+      .then(() => this.router.navigate(['/home'])); //geht nur zu Home wenn update erfolgreich
   }
 
   cancel(): void {
     this.router.navigate(['/home']);
   }
-
-
-
-
-
-
 }
-
-
