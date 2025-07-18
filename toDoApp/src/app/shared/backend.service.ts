fc256b7dc90263d4753f93f69afa481ac1d15bbc
Nadja Stauber <nadja.stauberahoo.de>
2025-07-18 15:59:56 +0200

    Backendservice angepasst - Login wir kommen

--- toDoApp/src/app/shared/backend.service.ts
+++ toDoApp/src/app/shared/backend.service.ts
@@ -12,7 +12,7 @@ export class BackendService {
 
   //get all
   async getAllToDos(): Promise<Todo[]> {
-    let response = await fetch(this.URL + '/todos');
+    let response = await fetch(this.URL + '/todos/todo');
     let todos = await response.json();
     console.log('todos in service (getAll) : ', todos)
     return todos;
@@ -20,7 +20,7 @@ export class BackendService {
 
 // get one
 async getOne(id: string): Promise<Todo> {
-  let response = await fetch(this.URL + '/todos/' + id);
+  let response = await fetch(this.URL + '/todos/todo/' + id);
   let todo = await response.json();
   console.log('todo in service (getOne) : ', todo)
   return todo;
@@ -28,7 +28,7 @@ async getOne(id: string): Promise<Todo> {
 
 //update one
 async update(id: string, updateData: Todo): Promise<Todo> {
-  let response = await fetch(this.URL + '/todos/' + id, {
+  let response = await fetch(this.URL + '/todos/todo/' + id, {
     method: "PATCH",      // Achtung, muss genau so heißen wie in Backend !
     body: JSON.stringify(updateData),
     headers: {
@@ -42,7 +42,7 @@ async update(id: string, updateData: Todo): Promise<Todo> {
 
 //post one
 async create(newData: Todo): Promise<Todo> {
-  let response = await fetch(this.URL + '/todos', {
+  let response = await fetch(this.URL + '/todos/todo', {
     method: "POST",      // Achtung, muss genau so heißen wie in Backend !
     body: JSON.stringify(newData),
     headers: {
@@ -58,7 +58,7 @@ async create(newData: Todo): Promise<Todo> {
 // delete one
 async deleteOne(id: string): Promise<{message: number}> {
 
-  let response = await fetch(this.URL + '/todos/' + id, {
+  let response = await fetch(this.URL + '/todos/todo/' + id, {
     method: "DELETE"
   });
   let status = await response.status;
