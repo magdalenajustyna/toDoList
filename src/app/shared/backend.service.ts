8bd8515aa09c138cf7571ec378d6d6d86a01e626
Nadja Stauber <nadja.stauber@yahoo.de>
2026-09-02 11:30:50 +0200

    use correct environment file for backendservice

--- src/app/shared/backend.service.ts
+++ src/app/shared/backend.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Todo } from './todo';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
